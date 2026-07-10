85caa13e3d54e312211514f76b15202cc020ec56
Arhaan khan <160334791+arhaann@users.noreply.github.com>
2026-07-11 00:19:52 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,11 @@
+<script>
+    // Navbar Scroll Effect
+    window.addEventListener('scroll', function() {
+        const navbar = document.getElementById('navbar');
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+</script>
